22d1c968d9e397b3c32df0f676eb5388fa36e28c
adeonita <adeonita.sousa@gmail.com>
2020-06-21 21:22:04 -0300

    Criando resolver para um atributo diferente

--- src/teste.js
+++ src/teste.js
@@ -3,7 +3,7 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
     return cooked;
 };
 var _a = require('apollo-server'), ApolloServer = _a.ApolloServer, gql = _a.gql;
-var typeDefs = gql(__makeTemplateObject(["\n    type Query {\n        ola: String\n        teste: Int\n    }\n"], ["\n    type Query {\n        ola: String\n        teste: Int\n    }\n"
+var typeDefs = gql(__makeTemplateObject(["\n    scalar Date #Novo escalar criado do tipo date\n\n\n    type User{\n        id: ID\n        nome: String!\n        email: String!\n        idade: Int\n        salario: Float\n        vip: Boolean\n    }\n\n    type Query {\n        ola: String\n        teste: Int\n        date: Date\n        user: User\n    }\n"], ["\n    scalar Date #Novo escalar criado do tipo date\n\n\n    type User{\n        id: ID\n        nome: String!\n        email: String!\n        idade: Int\n        salario: Float\n        vip: Boolean\n    }\n\n    type Query {\n        ola: String\n        teste: Int\n        date: Date\n        user: User\n    }\n"
     /**
      * A variavel resolver sendo do tipo myResolver
      */
@@ -12,9 +12,37 @@ var typeDefs = gql(__makeTemplateObject(["\n    type Query {\n        ola: Strin
  * A variavel resolver sendo do tipo myResolver
  */
 var resolvers = {
+    /**
+     * Resolver User para resolver a incompatibilidade
+     * do atributo salario x salario_liq
+     * O atributo retornado da base de dados é salario_liq
+     * enquanto o schema espera o atributo salário.
+     * O resolver abaixo retorna o valor de salario_liq para salario
+     */
+    User: {
+        salario: function (user) {
+            return user.salario_liq;
+        }
+    },
     Query: {
         ola: function () { return 'Retorno String'; },
-        teste: function () { return 123; }
+        teste: function () { return 123; },
+        date: function () { return new Date; },
+        user: function () {
+            //É possivel resolver o tipo usuario de 
+            //diversas maneiras, consultando uma api
+            //fazendo uma consulta no banco de dados
+            //ou simplesmente retornando um objeto como no 
+            //exemplo abaixo 
+            return {
+                id: 123,
+                nome: 'Ade',
+                email: 'adeonita.sousa@teste.com',
+                idade: 23,
+                salario_liq: 1235.67,
+                vip: true
+            };
+        }
     }
 };
 var server = new ApolloServer({ typeDefs: typeDefs, resolvers: resolvers });
--- src/teste.ts
+++ src/teste.ts
@@ -1,30 +1,87 @@
 const {ApolloServer, gql} = require('apollo-server')
 
+type User = {
+    id: number,
+    nome: string,
+    email: string,
+    idade: number, 
+    salario_liq: number, 
+    vip: boolean
+}
+
 /**
  * Defino o tipo do meu resolver
  */
-type myResolver = {  
+type MyResolver = {  
+    User:{
+        salario: (user: User) => number
+    },
     Query:{
         ola: () => string,
-        teste: () => number
+        teste: () => number,
+        date: () => Date, 
+        user: () => User //User retorna um valor do tipo User
     }
 }
 
+
 const typeDefs = gql`
+    scalar Date #Novo escalar criado do tipo date
+
+
+    type User{
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     type Query {
         ola: String
         teste: Int
+        date: Date
+        user: User
     }
 `
 /**
  * A variavel resolver sendo do tipo myResolver
  */
-const resolvers: myResolver = {
+const resolvers: MyResolver = {
+    /**
+     * Resolver User para resolver a incompatibilidade
+     * do atributo salario x salario_liq
+     * O atributo retornado da base de dados é salario_liq
+     * enquanto o schema espera o atributo salário.
+     * O resolver abaixo retorna o valor de salario_liq para salario
+     */
+    User: {
+        salario(user){
+           return user.salario_liq
+        }
+    },
     Query:{
         ola: () => 'Retorno String',
-        teste: () => 123
+        teste: () => 123,
+        date: () => new Date,
+        user() {
+            //É possivel resolver o tipo usuario de 
+            //diversas maneiras, consultando uma api
+            //fazendo uma consulta no banco de dados
+            //ou simplesmente retornando um objeto como no 
+            //exemplo abaixo 
+            return {
+                id: 123,
+                nome: 'Ade',
+                email: 'adeonita.sousa@teste.com',
+                idade: 23,
+                salario_liq: 1235.67,
+                vip: true
+            }
+        }
 
-    }
+    },
 }
 
 const server = new ApolloServer({typeDefs, resolvers})
